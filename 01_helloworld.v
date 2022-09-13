19655faeab43e067bf5b70ea20c7b1c5c3c75193
Revti Raman <hey.raman07@gmail.com>
2022-09-13 11:03:40 +0530

    Add files via upload
    First Program to work with Verilog. "Hello World"

---
+++ 01_helloworld.v
@@ -0,0 +1,10 @@
+//Tak_01 Hello World Example
+module hello_world ();
+
+//Display message in console on a new line with tab before
+initial begin
+    $display("\n\t Hello World! \n");
+    $display("\n\t End of the task! \n");        //Added another text after first simulation
+end
+    
+endmodule
