4e0a137ac759643ccdbc3130f40763da385c9ecd
Revti Raman <hey.raman07@gmail.com>
2022-10-24 01:25:17 +0530

    Add files via upload

---
+++ 3.02 Half Adder Dataflow/3.02a ha_dataflow.v
@@ -0,0 +1,9 @@
+module ha_dataflow (a,b,sum,carry);
+
+input a,b;
+output sum, carry;
+
+assign sum = a^b;
+assign carry = a&b;
+    
+endmodule
---
+++ 3.02 Half Adder Dataflow/3.02b ha_dataflow_tb.v
@@ -0,0 +1,25 @@
+`timescale 1ns/1ns
+module ha_structural_tb ();
+
+reg A, B;
+wire SUM, CARRY;
+
+//Instantiate the module
+ha_dataflow HA2(.a(A), .b(B), .sum(SUM), .carry(CARRY));
+
+//Monitor values
+initial begin
+    $monitor("A=%b, B=%b, SUM=%b, CARRY=%b", A, B, SUM, CARRY);
+end
+
+//Generate stimulus
+initial begin
+    A=0; B=0;
+    #1; A=0; B=1;
+    #1; A=1; B=0;
+    #1; A=1; B=1;
+    #1; A=0; B=0;
+
+end
+    
+endmodule
