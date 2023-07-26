6df7a501224168cbcbc9816769191cd848fc9637
Revti Raman <hey.raman07@gmail.com>
2023-07-26 10:47:18 +0530

    Update 8.01a single_port_SRAM.v

--- 8.01 Asynchronous Read SRAM/8.01a single_port_SRAM.v
+++ 8.01 Asynchronous Read SRAM/8.01a single_port_SRAM.v
@@ -1,8 +1,8 @@
 //Single port SRAM with asynchronous read
 //The size of the SRAM is 16x8bit words
-module single_SRAM (clk,data_in,data_out,address,write_en);
+module single_SRAM (clk,data_in,data_out,address,control);
 
-input clk,write_en;
+input clk,control;
 input [7:0]data_in;
 input [3:0]address;
 output [7:0]data_out;
@@ -15,7 +15,7 @@ reg [7:0]ram [0:15];
 
 always @(posedge clk) begin
     
-        if (write_en==1) begin
+        if (control==1) begin
             ram[address] <= data_in;
         end
 
